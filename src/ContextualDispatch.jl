fef13f0d7b1542ce2d92191e54a3d96106643c03
McCoy Becker <mccoybecker@gmail.com>
2021-05-01 10:21:50 -0400

    Clean up cassette.jl example from Mixtape and allow extension.

--- src/ContextualDispatch.jl
+++ src/ContextualDispatch.jl
@@ -1,5 +1,66 @@
 module ContextualDispatch
 
-greet() = print("Hello World!")
+using Mixtape
+using CodeInfoTools
+
+# Controls when the transform on lowered code applies. 
+# I don't want to apply the recursive transform in type inference.
+# So when stacklevel > 1, don't apply.
+# I'll just dispatch to `Mixtape.call` at runtime.
+@ctx (false, false, false) mutable struct Mix 
+    stacklevel::Int
+end
+Mix() = Mix(1)
+
+# Allow the transform on our Target module.
+allow(ctx::Mix, m::Module, fn, args...) = m == Target
+
+# Basically == a Cassette context.
+struct Context
+    d::Dict
+    Context() = new(Dict())
+end
+
+# Our version of overdub.
+overdub(::Context, f, args...) = f(args...)
+
+# The transform inserts state, then wraps calls in (overdub).
+# Then, anytime there's a return value --
+# create a tuple of (ret, state)
+# and returns that. 
+# Consider monadic lifting f: R -> R => trans => R -> (R, state).
+swap(r, e) = e
+function swap(r, e::Expr)
+    e.head == :call || return e
+    return Expr(:call, overdub, r, e.args[1:end]...)
+end
+
+function transform(mix::Mix, src)
+    b = CodeInfoTools.Builder(src)
+    mix.stacklevel == 1 || return
+    q = push!(b, Expr(:call, Context))
+    rets = Any[]
+    for (v, st) in b
+        b[v] = swap(q, st)
+        st isa Core.ReturnNode && push!(rets, v => st)
+    end
+    for (n, ret) in rets
+        v = insert!(b, n, Expr(:call, Base.tuple, ret.val, q))
+        b[n] = Core.ReturnNode(v)
+    end
+    mix.stacklevel += 1
+    return CodeInfoTools.finish(b)
+end
+
+# Optimize decrements the stacklevel. 
+# This honestly doesn't really matter, but it is good form.
+function postopt!(mix::Mix, ir)
+    mix.stacklevel -= 1
+    return ir
+end
+
+Mixtape.@load_call_interface()
+
+export call, overdub
 
 end # module
